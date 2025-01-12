653bab4a58718cc81ee39e37d862b6aa62c86886
James Carr <james.r.carr@gmail.com>
2025-01-12 07:46:42 -0600

    Add deallocation behavior to remove added lines

--- cosmicpython/models.py
+++ cosmicpython/models.py
@@ -11,11 +11,26 @@ class Batch:
     def __init__(self, reference, sku, qty, eta) -> None:
         self.reference = reference
         self.sku = sku
-        self.available_quantity = qty
         self.eta = eta
+        self._purchased_quantity = qty
+        self._allocations = set()
+
 
     def allocate(self, orderline: OrderLine) -> None:
-        self.available_quantity -= orderline.qty
+        if self.can_allocate(orderline):
+            self._allocations.add(orderline)
+
+    @property
+    def allocated_quantity(self) -> int:
+        return sum(line.qty for line in self._allocations)
+
+    @property
+    def available_quantity(self) -> int:
+        return self._purchased_quantity - self.allocated_quantity
+
+    def deallocate(self, line: OrderLine) -> None:
+        if line in self._allocations:
+            self._allocations.remove(line)
 
     def can_allocate(self, line: OrderLine) -> bool:
         return line.sku == self.sku and self.available_quantity >= line.qty
--- tests/test_model.py
+++ tests/test_model.py
@@ -3,6 +3,7 @@ from datetime import date
 
 
 def test_allocating_a_batch_reduces_the_avaialble_quantity():
+    batch, line = make_batch_and_line("SMALL-TABLE", 20, 2)
     batch = Batch("batch-001", "SMALL-TABLE", qty=20, eta=date.today())
     line = OrderLine("order-ref", "SMALL-TABLE", 2)
 
@@ -32,3 +33,20 @@ def test_cannot_allocate_if_skus_do_not_match():
     batch = Batch("batch-001", "UNCOMFORTABLE-CHAIR", 100, eta=None)
     different_sku_line = OrderLine("order-123", "EXPENSIVE-TOASTER", 10)
     assert batch.can_allocate(different_sku_line) is False
+
+def test_can_only_deallocate_removes_lines():
+    batch, line = make_batch_and_line("DECORATIVE-TRINKET", 20, 2)
+    batch.allocate(line)
+    batch.deallocate(line)
+    assert batch.available_quantity == 20
+
+def test_can_only_deallocate_allocated_lines():
+    batch, unallocated_line = make_batch_and_line("DECORATIVE-TRINKET", 20, 2)
+    batch.deallocate(unallocated_line)
+    assert batch.available_quantity == 20
+
+def test_allocation_is_idempotent():
+    batch, line = make_batch_and_line("ANGULAR-DESK", 20, 2)
+    batch.allocate(line)
+    batch.allocate(line)
+    assert batch.available_quantity == 18
