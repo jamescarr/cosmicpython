e0383aff73f6d5197618e4b0e38b837d6bc36691
James Carr <james.r.carr@gmail.com>
2025-01-13 08:05:58 -0600

    Remove unneeded except clause

--- cosmicpython/api.py
+++ cosmicpython/api.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI, Response, status
 from pydantic import BaseModel
-from cosmicpython.models import OrderLine, OutOfStock
 from cosmicpython.repository import SQLAlchemyRepository
 from cosmicpython import config, models, services
 
@@ -16,7 +15,7 @@ class AllocationRequest(BaseModel):
 
 @app.post("/allocate")
 def allocate(request: AllocationRequest, response: Response):
-    line = OrderLine(
+    line = models.OrderLine(
         request.orderid, request.sku, request.qty
     )
     session = get_session()
@@ -25,10 +24,7 @@ def allocate(request: AllocationRequest, response: Response):
         batchref = services.allocate(line, batches, session)
         response.status_code = status.HTTP_201_CREATED
         return {"batchref": batchref}
-    except OutOfStock as e:
-        response.status_code = status.HTTP_400_BAD_REQUEST
-        return {"message": str(e)}
-    except services.InvalidSku as e:
+    except (models.OutOfStock, services.InvalidSku) as e:
         response.status_code = status.HTTP_400_BAD_REQUEST
         return {"message": str(e)}
 
